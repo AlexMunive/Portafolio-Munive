02d2582bdb11f7f9bd4ef693a0b324c9a8a11829
AlexMunive <103223036+AlexMunive@users.noreply.github.com>
2022-07-13 10:38:21 -0500

    Portafolio-Alex

---
+++ scrip.js
@@ -0,0 +1,120 @@
+
+//  sección menú  //
+let sidebar = document.querySelector(".sidebar");
+let sidebarBtn = document.querySelector(".bx-menu");
+console.log(sidebarBtn);
+sidebarBtn.addEventListener("click", ()=>{
+  sidebar.classList.toggle("close");
+});
+
+// //  sección home //
+// let sidebarhome = document.querySelector(".sidebar");
+// let home = document.querySelector(".bx-home");
+// console.log(home);
+// home.addEventListener("click", ()=>{
+//   sidebarhome.classList.toggle("close");
+// });
+
+let sidebarhome2 = document.querySelector(".sidebar");
+let home2 = document.querySelector(".home1");
+console.log(home2);
+home2.addEventListener("click", ()=>{
+  sidebarhome2.classList.toggle("close");
+});
+
+// //  sección about //
+// let sidebarAbout = document.querySelector(".sidebar");
+// let about = document.querySelector(".bxs-user");
+// console.log(about);
+// about.addEventListener("click", ()=>{
+//   sidebarAbout.classList.toggle("close");
+// });
+
+let sidebarAbout2 = document.querySelector(".sidebar");
+let about2 = document.querySelector(".about1");
+console.log(about2);
+about2.addEventListener("click", ()=>{
+  sidebarAbout2.classList.toggle("close");
+});
+
+// //  sección resume //
+// let sidebarResume = document.querySelector(".sidebar");
+// let resume = document.querySelector(".bx-file-blank");
+// console.log(resume);
+// resume.addEventListener("click", ()=>{
+//   sidebarResume.classList.toggle("close");
+// });
+
+let sidebarResume2 = document.querySelector(".sidebar");
+let resume2 = document.querySelector(".resume1");
+console.log(resume2);
+resume2.addEventListener("click", ()=>{
+  sidebarResume2.classList.toggle("close");
+});
+
+
+
+
+// //  sección services //
+// let sidebarServices = document.querySelector(".sidebar");
+// let services = document.querySelector(".bx-server");
+// console.log(services);
+// services.addEventListener("click", ()=>{
+//   sidebarServices.classList.toggle("close");
+// });
+
+let sidebarServices2 = document.querySelector(".sidebar");
+let services2 = document.querySelector(".services1");
+console.log(services2);
+services2.addEventListener("click", ()=>{
+  sidebarServices2.classList.toggle("close");
+});
+
+
+// //  sección Portfolio //
+// let sidebarPortfolio = document.querySelector(".sidebar");
+// let portfolio = document.querySelector(".bxs-book-content");
+// console.log(portfolio);
+// portfolio.addEventListener("click", ()=>{
+//   sidebarPortfolio.classList.toggle("close");
+// });
+
+let sidebarPortfolio2 = document.querySelector(".sidebar");
+let portfolio2 = document.querySelector(".portfolio1");
+console.log(portfolio2);
+portfolio2.addEventListener("click", ()=>{
+  sidebarPortfolio2.classList.toggle("close");
+});
+
+
+// //  sección Blog //
+// let sidebarBlog = document.querySelector(".sidebar");
+// let blog = document.querySelector(".bxs-book-alt");
+// console.log(blog);
+// blog.addEventListener("click", ()=>{
+//   sidebarBlog.classList.toggle("close");
+// });
+
+let sidebarBlog2 = document.querySelector(".sidebar");
+let blog2 = document.querySelector(".blog1");
+console.log(blog2);
+blog2.addEventListener("click", ()=>{
+  sidebarBlog2.classList.toggle("close");
+});
+
+
+// //  sección contact //
+// let sidebarContact = document.querySelector(".sidebar");
+// let contact = document.querySelector(".bxs-contact");
+// console.log(contact);
+// contact.addEventListener("click", ()=>{
+//   sidebarContact.classList.toggle("close");
+// });
+
+let sidebarContact2 = document.querySelector(".sidebar");
+let contact2 = document.querySelector(".contact1");
+console.log(contact2);
+contact2.addEventListener("click", ()=>{
+  sidebarContact2.classList.toggle("close");
+});
+
